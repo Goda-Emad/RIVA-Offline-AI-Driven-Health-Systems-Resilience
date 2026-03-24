00475f129e13e3f90d7782dddc544c5f91f09017
goda emad <goda.emade2001@gmail.com>
2026-03-24 04:27:39 +0200

    Update sw.js

--- web-app/public/sw.js
+++ web-app/public/sw.js
@@ -1,1 +1,18 @@
+// sw.js - RIVA Service Worker
+const CACHE_NAME = 'riva-v1';
 
+self.addEventListener('install', (event) => {
+    console.log('[SW] Installing...');
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('[SW] Activating...');
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request).catch(() => {
+        return new Response('Offline', { status: 503 });
+    }));
+});
